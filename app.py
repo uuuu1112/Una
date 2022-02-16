29ff04b87897afe32f7578b2851309d18ced950f
uuuu1112 <uuuu1112@gmail.com>
2022-02-17 00:03:18 +0800

    build header

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,redirect,url_for,render_template
 
 app=Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html",content=name)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 
