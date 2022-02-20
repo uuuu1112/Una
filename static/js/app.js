2525d5c16a69915f72b10e22c5abe7ab2f33aba4
uuuu1112 <uuuu1112@gmail.com>
2022-02-20 12:56:10 +0800

    clear work

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,12 @@
 const navSlide=()=>{
     const burger = document.querySelector('.burger')
     const sidebarMenu=document.querySelector('.sidebarMenu')
+    const clear=document.querySelector('.clear')
 
     burger.addEventListener('click',()=>{
+        sidebarMenu.classList.toggle('sidebarMenu-active')
+    });
+    clear.addEventListener('click',()=>{
         sidebarMenu.classList.toggle('sidebarMenu-active')
     });
 }
