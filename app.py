079669e50a7fd6e10c6f018d0ec5c1e857513f93
uuuu1112 <uuuu1112@gmail.com>
2022-03-13 21:57:56 +0800

    add category

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,url_for
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/category")
+def category():
+    return render_template("category.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
 
 
 
