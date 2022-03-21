50278f7a12c8567c8703bdf552bb3251f254b2d5
uuuu1112 <uuuu1112@gmail.com>
2022-03-21 23:07:35 +0800

    add database

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
-from flask import redirect,url_for,render_template,url_for,request
-from models import db,app,Product
+from flask import redirect, url_for, render_template, url_for, request
+from models import db, app, Product
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/category")
 def category():
     return render_template("category.html")
+
 
 @app.route("/product")
 def product():
     return render_template("product.html")
 
+
 @app.route("/productlist")
 def productlist():
-    return render_template("productlist.html")
+    product=Product.query.all
+    return render_template("productlist.html",product=product)
 
-@app.route("/add",methods=['GET','POST'])
+
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    print(request.form)
+    if request.form:
+        print(request.form)
+        new_product = Product(name=request.form['name'], price=request.form['price'],
+                              size=request.form['size'], photo=request.form['photo'],
+                              category=request.form['category'], material=request.form['material'],
+                              caution=request.form['caution'], delivery=request.form['delivery'],
+                              alt=request.form['alt'])
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('productlist'))
     return render_template("add.html")
+
 
 @app.route("/edit")
 def edit():
     return render_template("edit.html")
+
 
 @app.route("/test")
 def test():
     return render_template("test.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -15,13 +15,15 @@ class Product(db.Model):
     price=db.Column('Price',db.String())
     size=db.Column('Size',db.String())
     photo=db.Column('Photo',db.String())
+    category=db.Column('Category',db.String())
     material=db.Column('Material',db.String())
     caution=db.Column('Caution',db.String())
-    category=db.Column('Category',db.String())
     delivery=db.Column('Delivery',db.String())
+    alt=db.Column('Alt',db.String())
 
     def __repr__(self):
         return f'''<Product Name: {self.name} Procuct Price:{self.price}
                     Size:{self.size} Photo:{self.photo}
                     Material:{self.material} Caution:{self.caution}
-                    Category:{self.category} Delivery:{self.delivery}'''
+                    Category:{self.category} Delivery:{self.delivery}
+                    Alt:{self.alt}'''
