cbda4db4c0c5808fc45d952e58d23ffd44554c00
uuuu1112 <uuuu1112@gmail.com>
2022-03-26 16:58:45 +0800

    add item to db

--- app.py
+++ app.py
@@ -19,11 +19,28 @@ def product():
 
 @app.route("/productlist")
 def productlist():
-    return render_template("productlist.html")
+    product=Product.query.all()
+    return render_template("productlist.html",product=product)
 
 
 @app.route("/add",methods=["GET","POST"])
 def add():
+    if request.form:
+        new_product=Product(
+            url=request.form['url'],
+            alt=request.form['alt'],
+            name=request.form['name'],
+            price=request.form['price'],
+            size=request.form['size'],
+            material=request.form['material'],
+            type=request.form['type'],
+            caution=request.form['caution'],
+            delivery=request.form['delivery'],
+            description=request.form['description']
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('productlist'))
     print(request.form)
     return render_template("add.html")
 
