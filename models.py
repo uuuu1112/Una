628e1fd7b776e20f01046c80805d7fc54e45a711
uuuu1112 <uuuu1112@gmail.com>
2022-03-26 16:20:16 +0800

    create data base

--- models.py
+++ models.py
@@ -11,19 +11,28 @@ db=SQLAlchemy(app)
 class Product(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     created=db.Column('Created',db.DateTime,default=datetime.datetime.now)
+    url=db.Column('url',db.String())
+    alt=db.Column('alt',db.String())
     name=db.Column('Name',db.String())
-    price=db.Column('Price',db.String())
-    size=db.Column('Size',db.String())
-    photo=db.Column('Photo',db.String())
-    category=db.Column('Category',db.String())
-    material=db.Column('Material',db.String())
-    caution=db.Column('Caution',db.String())
-    delivery=db.Column('Delivery',db.String())
-    alt=db.Column('Alt',db.String())
+    price=db.Column('price',db.String())
+    size=db.Column('size',db.String())
+    material=db.Column('material',db.String())
+    type=db.Column('type',db.String())
+    caution=db.Column('caution',db.String())
+    delivery=db.Column('delivery',db.String())
+    description=db.Column('description',db.Text)
+
+
 
     def __repr__(self):
-        return f'''<Product Name: {self.name} Procuct Price:{self.price}
-                    Size:{self.size} Photo:{self.photo}
-                    Material:{self.material} Caution:{self.caution}
-                    Category:{self.category} Delivery:{self.delivery}
-                    Alt:{self.alt}'''
+        return f'''<Product ( URL:{self.url}
+            ALT:{self.alt}
+            Name:{self.name}
+            Price:{self.price}
+            Size:{self.size}
+            Material:{self.material}
+            Type:{self.type}
+            Caution:{self.caution}
+            Delivery:{self.delivery}
+            Description:{self.description}
+            )'''
