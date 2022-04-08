b429dccfef659d89b48fe7e5dfeded3eb8dbe467
Jesse Tseng <pkh_uuuu1112@photons.com.tw>
2022-04-08 18:29:50 +0800

    減少一些檔案

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 from typing import NewType
 from flask import redirect, url_for, render_template, url_for, request
 from models import db, app, Product
-import detail
-import index
+import info
 
 
 
 @app.route("/")
 def home():
-    global index
-    productDetail=index.IndexItem.productDetail
-    newType=index.IndexItem.newType
-    return render_template("index.html",productDetail=productDetail,newType=newType)
+    global info
+    productDetail=info.IndexItem.productDetail
+    newType=info.IndexItem.newType
+    return render_template(
+        "index.html",
+        productDetail=productDetail,
+        newType=newType)
 
 
 @app.route("/category/<type>")
 def category(type):
     products=Product.query.filter_by(type=type).all()
-    return render_template("category.html",products=products)
+    global info
+    productDetail=info.IndexItem.productDetail
+    newType=info.IndexItem.newType
+    return render_template(
+        "category.html",
+        products=products,
+        productDetail=productDetail,
+        newType=newType)
 
 
 @app.route("/product/<id>")
 def product(id):
     product=Product.query.get(id)
-    global detail
-    new_detail=detail.Detail()
+    global info
+    new_detail=info.Detail()
     return render_template("product.html",product=product,detail=new_detail)
 
 
@@ -37,8 +46,8 @@ def productlist():
 @app.route("/add",methods=["GET","POST"])
 def add():
     if request.form:
-        global detail
-        new_detail=detail.Detail()
+        global info
+        new_detail=info.Detail()
         new_product=Product(
             url=request.form['url'],
             name=request.form['name'],
@@ -55,8 +64,8 @@ def add():
 
 @app.route("/edit/<id>",methods=['GET','POST'])
 def edit(id):
-    global detail
-    new_detail=detail.Detail()
+    global info
+    new_detail=info.Detail()
     product=Product.query.get(id)
     if request.form:
         product.name=request.form['name']
@@ -78,7 +87,7 @@ def delete(id):
 def test():
     # global detail
     # detail=detail.Detail()
-    return render_template("test.html",detail=detail)
+    return render_template("test.html",detail=info)
 
 
 
